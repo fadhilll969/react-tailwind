cc4597508c68c554f1d6f3d6c0784615dce1dcd6
fadhilll969 <sheyzz48@gmail.com>
2025-09-11 15:01:09 +0700

    tabel_tailwind

---
+++ pages/tabeltailwind.jsx
@@ -0,0 +1,150 @@
+import React from 'react'
+import { Table } from 'react-bootstrap';
+
+function Tabeltailwind() {
+
+    const data2 = [
+        { id: 1, nama_lengkap: "ambas", nama_belakang: "fadhil", username: "@padil" },
+        { id: 2, nama_lengkap: "rezake", nama_belakang: "arianto", username: "ryutee" },
+        { id: 3, nama_lengkap: "luminoir", nama_belakang: "lumi", username: "@noir" },
+    ];
+    return (
+        <>
+            <div className='grid grid-cols-3 -gap-4'>
+
+                <div style={{ width: "500px", marginLeft: "10%" }}>
+                    <h5>
+                        Tabel 1
+                    </h5>
+                    <table className="border-separate border border-gray-400" style={{ width: "400px" }}>
+                        <thead>
+                            <tr>
+
+                                <th class="border border-gray-300 ...">fullname</th>
+                                <th class="border border-gray-300 ...">lastname</th>
+                                <th class="border border-gray-300 ...">username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data2.map((item) => (
+                                <tr>
+                                    <td className="border border-gray-300">{item.nama_lengkap}</td>
+                                    <td className="border border-gray-300">{item.nama_belakang}</td>
+                                    <td className="border border-gray-300 m-8">{item.username}</td>
+                                </tr>
+                            ))}
+                        </tbody> 
+                    </table>
+                </div>
+
+
+                <div style={{ width: "350px", marginLeft: "10%" }}>
+                    <h5>
+                        Tabel 2
+                    </h5>
+                    <table className="border-separate border border-gray-400" style={{ width: "400px" }}>
+                        <thead>
+                            <tr>
+
+                                <th class="border border-gray-300 ...">fullname</th>
+                                <th class="border border-gray-300 ...">lastname</th>
+                                <th class="border border-gray-300 ...">username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data2.map((item) => (
+                                <tr>
+                                    <td className="border border-gray-300">{item.nama_lengkap}</td>
+                                    <td className="border border-gray-300">{item.nama_belakang}</td>
+                                    <td className="border border-gray-300 m-8">{item.username}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+
+
+                <div style={{ width: "350px", marginLeft: "10%" }}>
+                    <h5>
+                        Tabel 3
+                    </h5>
+                    <table className="border-separate border border-gray-400" style={{ width: "400px" }}>
+                        <thead>
+                            <tr>
+
+                                <th class="border border-gray-300 ...">fullname</th>
+                                <th class="border border-gray-300 ...">lastname</th>
+                                <th class="border border-gray-300 ...">username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data2.map((item) => (
+                                <tr>
+                                    <td className="border border-gray-300">{item.nama_lengkap}</td>
+                                    <td className="border border-gray-300">{item.nama_belakang}</td>
+                                    <td className="border border-gray-300 m-8">{item.username}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+
+
+                <div style={{ width: "350px", marginLeft: "60%" }}>
+                    <h5>
+                        Tabel 4
+                    </h5>
+                    <table className="border-separate border border-gray-400" style={{ width: "400px" }}>
+                        <thead>
+                            <tr>
+
+                                <th class="border border-gray-300 ...">fullname</th>
+                                <th class="border border-gray-300 ...">lastname</th>
+                                <th class="border border-gray-300 ...">username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data2.map((item) => (
+                                <tr>
+                                    <td className="border border-gray-300">{item.nama_lengkap}</td>
+                                    <td className="border border-gray-300">{item.nama_belakang}</td>
+                                    <td className="border border-gray-300 m-8">{item.username}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+
+
+                <div style={{ width: "350px", marginLeft: "60%" }}>
+                    <h5>
+                        Tabel 5
+                    </h5>
+                    <table className="border-separate border border-gray-400" style={{ width: "400px" }}>
+                        <thead>
+                            <tr>
+
+                                <th class="border border-gray-300 ...">fullname</th>
+                                <th class="border border-gray-300 ...">lastname</th>
+                                <th class="border border-gray-300 ...">username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data2.map((item) => (
+                                <tr>
+                                    <td className="border border-gray-300">{item.nama_lengkap}</td>
+                                    <td className="border border-gray-300">{item.nama_belakang}</td>
+                                    <td className="border border-gray-300 m-8">{item.username}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+
+
+            </div>
+        </>
+    )
+}
+
+export default Tabeltailwind
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom"
+import Tabeltailwind from "../pages/tabeltailwind"
 
 const App = () => {
   return (
-    <div>
-      <div className='flex h-screen justify-center items-center'>
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-          <img className="w-full" src="https://thvnext.bing.com/th/id/OIP.A5IwNFVdzGG3PZk79MMo8AHaHa?w=174&h=180&c=7&r=0&o=7&cb=ucfimgc2&pid=1.7&rm=3"alt="Sunset in the mountains" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">hy tkasingh</div>
-            <p className="text-gray-700 text-base">
-             yaping final boss🥀
-            </p>
-          </div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#lol</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#haiguys</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#wow</span>
-          </div>
-        </div>
+<Routes>
+  <Route path="/" element={<Tabeltailwind/>}/>
+</Routes>
 
-      </div>
-    </div>
   )
 }
-
 export default App
